1a8bde878f181933ff33595280c883ca22954a60
Ngai Lam Chou <ngai.lam.chou@gmail.com>
2024-04-05 10:34:41 -0700

    removed sqrt and sq

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 let temp;
 
 function appendValue(val) {
-  if (typeof temp === 'undefined' || document.getElementById('result').value === '0') {
-    temp = val;
-  } else {
-    temp = temp + val;
-  }
-  document.getElementById('result').value = temp;
+    if (typeof temp === "undefined" || temp === 0) {
+        temp = val;
+    } else {
+        temp = temp + val;
+    }
+    document.getElementById("result").value = temp;
 }
 
 function calculate() {
-  temp = eval(temp);
-  temp = Number(temp.toFixed(11)); 
-  document.getElementById('result').value = temp;
+    temp = eval(temp);
+    document.getElementById("result").value = temp;
 }
 
 function deleteLast() {
-  temp = temp.slice(0, -1);
-  document.getElementById('result').value = temp;
+    temp = temp.slice(0, -1);
+    document.getElementById("result").value = temp;
 }
 
 function clearResult() {
-  temp = 0;
-  document.getElementById('result').value = temp;
+    temp = 0;
+    document.getElementById("result").value = temp;
 }
 
 function sqrt() {
     temp = Math.sqrt(temp);
     temp = Number(temp.toFixed(11));
-    document.getElementById('result').value = temp;
+    document.getElementById("result").value = temp;
 }
 
 function sq() {
     temp = Math.pow(temp, 2);
     temp = Number(temp.toFixed(11));
-    document.getElementById('result').value = temp;
+    document.getElementById("result").value = temp;
 }
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+      temp = document.getElementById("result").value.toString();
+      calculate();
+  }
+});

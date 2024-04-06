2bc5a9f96d44d599623d68b0b1a56f2a319d01b6
Ngai Lam Chou <ngai.lam.chou@gmail.com>
2024-04-05 18:14:51 -0700

    add notepad

--- script.js
+++ script.js
@@ -1,44 +1,81 @@
-let temp;
+let temp = "";
 
 function appendValue(val) {
-    if (typeof temp === "undefined" || temp === 0) {
-        temp = val;
-    } else {
-        temp = temp + val;
-    }
-    document.getElementById("result").value = temp;
+  temp = temp + val;
+  document.getElementById("result").value = temp;
 }
 
 function calculate() {
-    temp = eval(temp);
-    document.getElementById("result").value = temp;
+  temp = eval(temp);
+  document.getElementById("result").value = temp;
 }
 
 function deleteLast() {
-    temp = temp.slice(0, -1);
-    document.getElementById("result").value = temp;
+  temp = temp.slice(0, -1);
+  document.getElementById("result").value = temp;
 }
 
 function clearResult() {
-    temp = 0;
-    document.getElementById("result").value = temp;
-}
-
-function sqrt() {
-    temp = Math.sqrt(temp);
-    temp = Number(temp.toFixed(11));
-    document.getElementById("result").value = temp;
-}
-
-function sq() {
-    temp = Math.pow(temp, 2);
-    temp = Number(temp.toFixed(11));
-    document.getElementById("result").value = temp;
+  temp = 0;
+  document.getElementById("result").value = temp;
 }
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-      temp = document.getElementById("result").value.toString();
-      calculate();
+    temp = document.getElementById("result").value.toString();
+    calculate();
   }
 });
+
+function add() {
+  ol = document.querySelector("ol");
+  let li = document.createElement("li");
+  calculate();
+  li.textContent = document.getElementById("result").value;
+  li.classList.add("clickable");
+  li.onclick = function () {
+    var value = this.textContent;
+    appendValue(value);
+  };
+  ol.appendChild(li);
+  clearResult();
+}
+
+function clr() {
+  let li = document.querySelectorAll("li");
+  li.forEach(function (element) {
+    element.remove();
+  });
+}
+
+function chg() {
+  let liItems = document.querySelectorAll("li");
+  liItems.forEach(function (li) {
+    li.onclick = function () {
+      this.textContent = document.getElementById("result").value;
+    };
+  });
+}
+
+function del() {
+  let liItems = document.querySelectorAll("li");
+  liItems.forEach(function (li) {
+    li.onclick = function () {
+      li.remove();
+      li.classList.remove("clickable");
+    };
+    li.classList.add("clickable");
+  });
+}
+
+var listItems = document.querySelectorAll("li");
+
+function addtor() {
+  listItems.forEach(function (listItem) {
+    listItem.addEventListener("click", function () {
+      var value = this.innerText;
+      console.log(value);
+      appendValue(value);
+    });
+  });
+}
